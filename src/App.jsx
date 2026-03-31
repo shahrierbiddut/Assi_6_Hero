32ef6dfa3e0278a6aad3759ba95ba925bc010c09
Md Shahrier Hossain Biddut <biddut22205101188@diu.edu.bd>
2026-03-31 22:44:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h2 className='text-white text-center font-bold bg-green-900 text-3xl'>Hello</h2>
+    </>
+  )
+}
+
+export default App
